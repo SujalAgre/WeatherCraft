aae93bb5427ec8d785d7b81033bf469423625a98
unknown <sujalagre26@gmail.com>
2025-04-07 01:22:08 +0530

    fix jukebox

--- src/jukebox.js
+++ src/jukebox.js
@@ -1,35 +1,35 @@
 const jukebox = [
     {
         name: 'Cat - Minecraft Music Disc - C418',
-        location: 'src/assets/music/minecraft-music/Cat - Minecraft Music Disc - C418.mp3'
+        location: './src/assets/music/minecraft-music/Cat - Minecraft Music Disc - C418.mp3'
     },
     {
         name: 'Chirp - Minecraft Music Disc - C418',
-        location: 'src/assets/music/minecraft-music/Chirp - Minecraft Music Disc - C418.mp3'
+        location: './src/assets/music/minecraft-music/Chirp - Minecraft Music Disc - C418.mp3'
     },
     {
         name: 'Far - Minecraft Music Disc - C418',
-        location: 'src/assets/music/minecraft-music/Far - Minecraft Music Disc - C418.mp3'
+        location: './src/assets/music/minecraft-music/Far - Minecraft Music Disc - C418.mp3'
     },
     {
         name: 'Mall - Minecraft Music Disc - C418',
-        location: 'src/assets/music/minecraft-music/Mall - Minecraft Music Disc - C418.mp3'
+        location: './src/assets/music/minecraft-music/Mall - Minecraft Music Disc - C418.mp3'
     },
     {
         name: 'Mellohi - Minecraft Music Disc - C418',
-        location: 'src/assets/music/minecraft-music/Mellohi - Minecraft Music Disc - C418.mp3'
+        location: './src/assets/music/minecraft-music/Mellohi - Minecraft Music Disc - C418.mp3'
     },
     {
         name: 'Otherside - Minecraft Music Disc - Lena Raine',
-        location: 'src/assets/music/minecraft-music/Otherside - Minecraft Music Disc - Lena Raine.mp3'
+        location: './src/assets/music/minecraft-music/Otherside - Minecraft Music Disc - Lena Raine.mp3'
     },
     {
         name: 'Pigstep -  Minecraft Music Disc - Lena Raine',
-        location: 'src/assets/music/minecraft-music/Pigstep -  Minecraft Music Disc - Lena Raine.mp3'
+        location: './src/assets/music/minecraft-music/Pigstep -  Minecraft Music Disc - Lena Raine.mp3'
     },
     {
         name: 'Relic - Minecraft Music Disc - Aaron Cherof',
-        location: 'src/assets/music/minecraft-music/Relic - Minecraft Music Disc - Aaron Cherof.mp3'
+        location: './src/assets/music/minecraft-music/Relic - Minecraft Music Disc - Aaron Cherof.mp3'
     },
     {
         name: 'Stal- Minecraft Music Disc - C418',
@@ -37,15 +37,15 @@ const jukebox = [
     },
     {
         name: 'Strad - Minecraft Music Disc - C418',
-        location: 'src/assets/music/minecraft-music/Strad - Minecraft Music Disc - C418.mp3'
+        location: './src/assets/music/minecraft-music/Strad - Minecraft Music Disc - C418.mp3'
     },
     {
         name: 'Wait - Minecraft Music Disc - C418',
-        location: 'src/assets/music/minecraft-music/Wait - Minecraft Music Disc - C418.mp3'
+        location: './src/assets/music/minecraft-music/Wait - Minecraft Music Disc - C418.mp3'
     },
     {
         name: 'Ward - Minecraft Music Disc - C418',
-        location: 'src/assets/music/minecraft-music/Ward - Minecraft Music Disc - C418.mp3'
+        location: './src/assets/music/minecraft-music/Ward - Minecraft Music Disc - C418.mp3'
     },
 ]
 
